ea0f5acf5afc536d5133db02694a2be6bdd071fc
bryan_alvarado <bryanalvarado1904@gmail.com>
2023-10-13 07:44:36 -0600

    comentario nuevo

--- polls/tests.py
+++ polls/tests.py
@@ -1,5 +1,5 @@
+#buenos dias
 import datetime
-
 from django.test import TestCase
 
 from django.utils import timezone
@@ -7,7 +7,6 @@ from django.utils import timezone
 from django.urls import reverse
 
 from .models import Question
-
 # Create your tests here.
 class QuestionModelTests(TestCase):
     def test_was_published_recently_with_future_question(self):
